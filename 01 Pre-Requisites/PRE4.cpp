2c96b4522ac9b37bd0ac99cac76bb683093bf5d9
Pranjal Gupta <pranjalg131@gmail.com>
2020-09-17 13:29:52 +0530

    Problems fixed

--- 01 Pre-Requisites/PRE4.cpp
+++ 01 Pre-Requisites/PRE4.cpp
@@ -35,8 +35,13 @@ int main()
   int n;
   cin >> n;
   int arr[n];
-  
+
   for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  
+  for (int i = 0; i < n/2; i++)
   {
     int groups = (arr[i] + arr[n-i-1]) / 10;
     int left = (arr[i] + arr[n-i-1]) % 10;
